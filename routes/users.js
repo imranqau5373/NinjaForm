c6e936a64b3d920bed65a35bdd602bd6a2003d72
Imran Khan <imranqau5373@gmail.com>
2019-12-15 00:34:55 +0500

    new commit

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require("request");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,10 +8,33 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/NinjaForm', function(req, res, next) {
-  console.log('contact form data.');
-  var contactFromData = req.body;
-  console.log(contactFromData);
-  res.send('respond with a resource');
+  var options = { method: 'POST',
+  url: 'https://oauth.wildapricot.org/auth/token',
+  headers: 
+   { 'cache-control': 'no-cache',
+     Connection: 'keep-alive',
+     'Content-Length': '40',
+     'Accept-Encoding': 'gzip, deflate',
+     Host: 'oauth.wildapricot.org',
+     'Postman-Token': '0120cde3-3530-44e3-9c58-dfb6a0f94b2b,0ec0cc23-3db9-43a5-99a3-d830b6e1a88d',
+     'Cache-Control': 'no-cache',
+     'User-Agent': 'PostmanRuntime/7.20.1',
+     Authorization: 'Basic QVBJS0VZOjdzYTQ0bnVsemtiemxqbGw4dTE4MnM4OXhpb2thNQ==',
+     Accept: 'application/json',
+     'Content-Type': 'application/x-www-form-urlencoded' },
+  form: { grant_type: 'client_credentials', scope: 'auto' } };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+  
+
+  console.log(body);
 });
+});
+
+
+
+
+
 
 module.exports = router;
