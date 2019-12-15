c86afbd81ea5ea0cd5a89da2b0502fbd930c241b
Imran Khan <imranqau5373@gmail.com>
2019-12-15 13:47:22 +0500

    new commit

--- routes/users.js
+++ routes/users.js
@@ -4,57 +4,14 @@ var request = require("request");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  const oauthTokenUrl = 'https://oauth.wildapricot.org/auth/token';
-  const apiBaseUrl = 'https://api.wildapricot.org/v2/';
-  
-  const api_key = '7sa44nulzkbzljll8u182s89xioka5';
-  const accountId = 307033;
-  let contactData = req.body;
-  console.log(new Buffer(`APIKEY:${api_key}`).toString('base64'));
-  request.post(oauthTokenUrl, {
-    form: {
-      grant_type: 'client_credentials',
-      scope: 'auto'
-    },
-    headers: {
-       'content-type': 'application/x-www-form-urlencoded',
-       'Accept': 'application/json',
-       "Authorization": "Basic " + new Buffer(`APIKEY:${api_key}`).toString('base64')
-     },
-    json: true
-  }, function (err, wilres, body) {
-    let accessToken = body.access_token;
-    console.log('access token is working',accessToken);
-    var options = { method: 'POST',
-  url: 'https://api.wildapricot.org/v2/accounts/307033/Contacts/',
-  headers: 
-   { 'cache-control': 'no-cache',
-     Connection: 'keep-alive',
-     'Content-Length': '61',
-     'Accept-Encoding': 'gzip, deflate',
-     Host: 'api.wildapricot.org',
-     'Postman-Token': '2f53ba2e-244a-41a8-857a-ab29f2c27b2d,5fd39c14-ef1f-41c2-b9d7-ad8a12273d50',
-     'Cache-Control': 'no-cache',
-     'User-Agent': 'PostmanRuntime/7.20.1',
-     'Content-Type': 'application/x-www-form-urlencoded',
-     Authorization: 'Bearer '+accessToken,
-     Accept: 'application/json' },
-  form: 
-   { Email: contactData.Email,
-     FirstName: contactData.FirstName,
-     LastName: contactData.LastName,
+  console.log(req.body);
+  res.json('it is working.')
 
-   } 
-  };
+});
 
-    request(options, function (error, response, body) {
-      if (error) throw new Error(error);
-
-      console.log(body);
-      console.log('after wil api requst.');
-      res.json('It is working now.');
-    });
-  })
+router.post('/TestForm', function(req, res, next) {
+  console.log(req.body);
+  res.json('it is working.')
 
 });
 
@@ -65,6 +22,10 @@ router.post('/NinjaForm', function(req, res, next) {
   const api_key = '7sa44nulzkbzljll8u182s89xioka5';
   const accountId = 307033;
   let contactData = req.body;
+  if(contactData.MemberShip == "")
+      contactData.MemberShipId = "";
+  else
+      contactData.MemberShipId = "";
   console.log(new Buffer(`APIKEY:${api_key}`).toString('base64'));
   request.post(oauthTokenUrl, {
     form: {
@@ -98,6 +59,8 @@ router.post('/NinjaForm', function(req, res, next) {
    { Email: contactData.Email,
      FirstName: contactData.FirstName,
      LastName: contactData.LastName,
+     'MembershipLevel.Id': contactData.MemberShipId,
+     MembershipEnabled : true
 
    } 
   };
