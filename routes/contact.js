a778ca54e530164cfcd6e99f16b44d5d1eaa9ea0
imranqau5373 <imranqau5373@gmail.com>
2019-12-19 22:57:13 +0500

    nnh

--- routes/contact.js
+++ routes/contact.js
@@ -10,35 +10,39 @@ function sayHi() {
 /* GET users listing. */
 router.post('/addcontact', function(req, res, next) {
     fs.readFile('token.txt', 'utf8', function(err, token) {
-        setTimeout(sayHi, 10000);
-        var options = { method: 'POST',
-        url: 'https://api.wildapricot.org/v2/accounts/307033/Contacts/',
-        headers:
-         { 'cache-control': 'no-cache',
-           Connection: 'keep-alive',
-           'Content-Length': '72',
-           'Accept-Encoding': 'gzip, deflate',
-           Host: 'api.wildapricot.org',
-           'Postman-Token': '454a8e75-ad75-4e3c-9943-99c9c5928b5c,9b99ddf4-af7f-4432-a9c6-8299f56cff91',
-           'Cache-Control': 'no-cache',
-           'User-Agent': 'PostmanRuntime/7.20.1',
-           'Content-Type': 'application/x-www-form-urlencoded',
-           Authorization: 'Bearer ' + token,
-           Accept: 'application/json' },
-        form:
-         {
-           Email: req.Email,
-           FirstName: req.FirstName,
-           LastName: req.LastName
-         }
-         };
-      request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        console.log('save contact response');
-        console.log(body);
-        res.json(body);
-        });
+        console.log('in add contact.');
+    var options = { method: 'POST',
+    url: 'https://api.wildapricot.org/v2/accounts/307033/Contacts/',
+    headers:
+     { 'cache-control': 'no-cache',
+       Connection: 'keep-alive',
+       'Content-Length': '72',
+       'Accept-Encoding': 'gzip, deflate',
+       Host: 'api.wildapricot.org',
+       'Postman-Token': '454a8e75-ad75-4e3c-9943-99c9c5928b5c,9b99ddf4-af7f-4432-a9c6-8299f56cff91',
+       'Cache-Control': 'no-cache',
+       'User-Agent': 'PostmanRuntime/7.20.1',
+       'Content-Type': 'application/x-www-form-urlencoded',
+       Authorization: 'Bearer ' + token,
+       Accept: 'application/json' },
+    form:
+     {
+       Email: req.Email,
+       FirstName: req.FirstName,
+       LastName: req.LastName,
+       'MembershipLevel.Id': 1109926,
+       MembershipEnabled: true
+     }
+     };
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    console.log('save contact response');
+    console.log(body);
+    res.json(body);
     });
+       
+    });
+    
 
 });
 
